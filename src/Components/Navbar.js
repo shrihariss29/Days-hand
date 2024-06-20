a8530f0a9b5f6cac79b07926f425d24484262aa4
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-06-20 14:10:33 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,9 +3,13 @@ import { Outlet, useLocation } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { useState } from 'react';
 function Navb() {
   const location = useLocation();
+  const [isToggled, setIsToggled] = useState(false);
+  const handleToggle = () => {
+    setIsToggled(!isToggled);
+  };
 
 
   const showBrand = location.pathname !== '/';
@@ -18,12 +22,15 @@ function Navb() {
           <Navbar.Brand className='opname' href="/"><span style={{ color: '#10439f' }}>Day</span><span style={{ color: 'red' }}>s h</span>
           <span style={{ color: '#10439f' }}>and</span></Navbar.Brand>
         )}
-        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto custom-toggle" onClick={handleToggle}>
+            <span className='toggleletters'>{isToggled ? 'X Close' : ' Menu'}</span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link className="navele" href="Add">Add</Nav.Link>
-            <Nav.Link className="navele" href="Subtract">Subtract</Nav.Link>
-            <Nav.Link className="navele" href="Count">Count</Nav.Link>
+            <Nav.Link id="navele0" className="navele element" href="/">Home</Nav.Link>
+            <Nav.Link id="navele1" className="navele element" href="/Subtract">Subtract</Nav.Link>
+            <Nav.Link id="navele2" className="navele element" href="/Add">Add</Nav.Link>
+            <Nav.Link id="navele3" className="navele element" href="/Count">Count</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
