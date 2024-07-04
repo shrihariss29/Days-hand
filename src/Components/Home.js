41474484602a893eb41f009e757adbe22119ff59
Shrihari <shrihariss29@gmail.com>
2024-07-04 14:25:56 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <div>
       <h1 id='name'>Day<span style={{ color: 'red' }}>s </span><span style={{ color: 'red' }}>h</span>and</h1>
       <div className='intro'>
-        <h1 className='intro1'>Time is preciousssss</h1>
+        <h1 className='intro1'>Time is precious</h1>
         <h4 className='intro1'>Calculate your dates and know how much it means to you.</h4>
         <ul>
           <li>You can add specific number of days to a day.</li>
